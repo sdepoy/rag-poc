f807efa332cfe867b1382e545ac8cd5364037909
Stu DePoy <stu.depoy@thinkcompany.com>
2025-10-21 11:38:45 -0400

    Added config file and fixed some package dependencies

---
+++ config.py
@@ -0,0 +1,7 @@
+# --- Data and Model Paths ---
+CLEANED_DATA_FILE="/Users/studepoy/Documents/Python/source/cleaned_data.txt"
+MODEL_FILE_PATH="/Users/studepoy/Documents/Python/Models/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf"
+
+# --- Model Parameters ---
+MODEL_N_CTX = 2048
+MODEL_TEMP = 0.3
